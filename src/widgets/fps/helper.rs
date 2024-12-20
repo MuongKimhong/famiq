3c6e2180877be231bbdc3d5b260ca2e2f20aecd8
MuongKimhong <muongkimhong086@gmail.com>
2024-12-21 10:08:33 +1100

    rewrite fps

---
+++ src/widgets/fps/helper.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::*;
+
+// top left corner
+pub fn default_fps_text_container_node() -> Node {
+    Node {
+        position_type: PositionType::Absolute,
+        width: Val::Auto,
+        height: Val::Auto,
+        left: Val::Px(5.0),
+        top: Val::Px(5.0),
+        ..default()
+    }
+}
--- src/widgets/fps/mod.rs
+++ src/widgets/fps/mod.rs
@@ -1,14 +1,21 @@
-use crate::utils::strip_assets_prefix;
-use crate::widgets::{DefaultTextBundle, FaWidgetBundle, FamiqWidgetId};
+pub mod helper;
+
+use crate::utils::{entity_add_child, strip_assets_prefix};
+use crate::widgets::{DefaultTextEntity, DefaultWidgetEntity, FamiqWidgetId};
 use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
 use bevy::ecs::system::EntityCommands;
 use bevy::prelude::*;
+use helper::*;
 
-// TextBundle Wrapped inside FaWidgetBundle
+use super::color::{GREEN_COLOR, WHITE_COLOR};
+
 const DEFAULT_FPS_TEXT_SIZE: f32 = 20.0;
 
 #[derive(Component)]
-pub struct IsFamiqFPSText;
+pub struct IsFamiqFPSTextLabel;
+
+#[derive(Component)]
+pub struct IsFamiqFPSTextCount;
 
 #[derive(Component)]
 pub struct IsFamiqFPSTextContainer;
@@ -16,63 +23,88 @@ pub struct IsFamiqFPSTextContainer;
 pub struct FaFpsText;
 
 impl<'a> FaFpsText {
-    fn _build_fps_text_container(id: &str, root_node: &'a mut EntityCommands) -> Entity {
-        let container_bundle = FaWidgetBundle {
-            style: Style {
-                position_type: PositionType::Absolute,
-                width: Val::Auto,
-                height: Val::Auto,
-                left: Val::Px(5.0),
-                top: Val::Px(5.0),
-                ..default()
-            },
-            ..default()
-        };
+    fn _build_container(id: &str, root_node: &'a mut EntityCommands) -> Entity {
+        let node = default_fps_text_container_node();
+        let border_color = BorderColor::default();
+        let border_radius = BorderRadius::default();
+        let bg_color = BackgroundColor::default();
+        let z_index = ZIndex(50);
+        let visibility = Visibility::Visible;
+
         root_node
             .commands()
             .spawn((
-                container_bundle,
-                IsFamiqFPSTextContainer,
+                node.clone(),
+                border_color.clone(),
+                border_radius.clone(),
+                bg_color.clone(),
+                z_index.clone(),
+                visibility.clone(),
                 FamiqWidgetId(format!("{id}_fps_text_container")),
+                IsFamiqFPSTextContainer,
+                DefaultWidgetEntity::new(
+                    node,
+                    border_color,
+                    border_radius,
+                    bg_color,
+                    z_index,
+                    visibility,
+                ),
             ))
             .id()
     }
 
-    fn _build_fps_text(
+    fn _build_text(
         id: &str,
         root_node: &'a mut EntityCommands,
         asset_server: &'a ResMut<'a, AssetServer>,
         font_path: &String,
     ) -> Entity {
-        let path = strip_assets_prefix(font_path).unwrap();
+        let label_txt = Text::new("FPS: ");
+        let label_txt_font = TextFont {
+            font: asset_server.load(strip_assets_prefix(font_path).unwrap()),
+            font_size: DEFAULT_FPS_TEXT_SIZE,
+            ..default()
+        };
+        let label_txt_color = TextColor(WHITE_COLOR);
+        let label_txt_layout = TextLayout::new_with_justify(JustifyText::Center);
 
-        let fps_label = TextSection::new(
-            "FPS: ",
-            TextStyle {
-                font: asset_server.load(&path),
-                font_size: DEFAULT_FPS_TEXT_SIZE,
-                ..default()
-            },
-        );
-        let fps_count = TextSection::new(
-            "0",
-            TextStyle {
-                font: asset_server.load(&path),
-                font_size: DEFAULT_FPS_TEXT_SIZE,
-                ..default()
-            },
-        );
-        let fps_text_bundle = TextBundle::from_sections([fps_label.clone(), fps_count.clone()]);
+        let count_txt = TextSpan::default();
+        let count_txt_font = label_txt_font.clone();
+        let count_txt_color = TextColor(GREEN_COLOR);
+        let count_txt_layout = TextLayout::new_with_justify(JustifyText::Center);
 
-        root_node
+        let label_txt_entity = root_node
             .commands()
             .spawn((
-                fps_text_bundle,
-                IsFamiqFPSText,
+                label_txt.clone(),
+                label_txt_font.clone(),
+                label_txt_color.clone(),
+                label_txt_layout.clone(),
                 FamiqWidgetId(id.to_string()),
-                DefaultTextBundle(TextBundle::from_sections([fps_label, fps_count])),
+                DefaultTextEntity::new(
+                    label_txt,
+                    label_txt_font,
+                    label_txt_color,
+                    label_txt_layout,
+                ),
+                IsFamiqFPSTextLabel,
             ))
-            .id()
+            .id();
+
+        let count_txt_entity = root_node
+            .commands()
+            .spawn((
+                count_txt,
+                count_txt_font,
+                count_txt_color,
+                count_txt_layout,
+                IsFamiqFPSTextCount,
+            ))
+            .id();
+
+        entity_add_child(root_node, count_txt_entity, label_txt_entity);
+        label_txt_entity
     }
 
     pub fn new(
@@ -81,27 +113,23 @@ impl<'a> FaFpsText {
         asset_server: &'a ResMut<'a, AssetServer>,
         font_path: &String,
     ) -> Entity {
-        let container_entity = Self::_build_fps_text_container(id, root_node);
-        let fps_entity = Self::_build_fps_text(id, root_node, asset_server, font_path);
+        let container_entity = Self::_build_container(id, root_node);
+        let text_entity = Self::_build_text(id, root_node, asset_server, font_path);
 
-        root_node
-            .commands()
-            .entity(container_entity)
-            .add_child(fps_entity);
-
-        fps_entity
+        entity_add_child(root_node, text_entity, container_entity);
+        text_entity
     }
 
-    pub fn update_fps_count_system(
-        diagnostics: Res<DiagnosticsStore>,
-        mut query: Query<&mut Text, With<IsFamiqFPSText>>,
-    ) {
-        for mut text in &mut query {
-            if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
-                if let Some(value) = fps.smoothed() {
-                    text.sections[1].value = format!("{value:.2}");
-                }
-            }
-        }
-    }
+    //     pub fn update_fps_count_system(
+    //         diagnostics: Res<DiagnosticsStore>,
+    //         mut query: Query<&mut Text, With<IsFamiqFPSText>>,
+    //     ) {
+    //         for mut text in &mut query {
+    //             if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
+    //                 if let Some(value) = fps.smoothed() {
+    //                     text.sections[1].value = format!("{value:.2}");
+    //                 }
+    //             }
+    //         }
+    //     }
 }
