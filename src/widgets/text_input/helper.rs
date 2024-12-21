06833ee5dcecb3ae93f9fc9a160dc3a93fecbaa6
MuongKimhong <muongkimhong086@gmail.com>
2024-12-21 14:45:10 +1100

    rewrite text_input widget

--- src/widgets/text_input/helper.rs
+++ src/widgets/text_input/helper.rs
@@ -1,56 +1,76 @@
 use crate::widgets::text_input::*;
-use crate::widgets::FaWidgetBundle;
+// use crate::widgets::FaWidgetBundle;
 use bevy::prelude::*;
 
-fn set_text_input_default_size(
-    size: &Option<TextInputSize>,
-    // text_bundle: &TextBundle,
-    text_input_bundle: &mut FaWidgetBundle,
-) {
-    let width = Val::Percent(100.0);
-    let size_small_height = Val::Px(28.0);
-    let size_normal_height = Val::Px(34.0);
-    let size_large_height = Val::Px(40.0);
+pub const PLACEHOLDER_COLOR: Color = Color::srgba(0.651, 0.651, 0.651, 0.6);
+pub const TEXT_INPUT_VALUE_COLOR: Color = Color::srgba(1.0, 1.0, 1.0, 0.922);
 
-    let height = match size {
-        Some(TextInputSize::Small) => size_small_height,
-        Some(TextInputSize::Normal) => size_normal_height,
-        Some(TextInputSize::Large) => size_large_height,
-        None => size_normal_height,
-    };
+// fn set_text_input_default_size(
+//     size: &Option<TextInputSize>,
+//     // text_bundle: &TextBundle,
+//     text_input_bundle: &mut FaWidgetBundle,
+// ) {
+//     let width = Val::Percent(100.0);
+//     let size_small_height = Val::Px(28.0);
+//     let size_normal_height = Val::Px(34.0);
+//     let size_large_height = Val::Px(40.0);
 
-    text_input_bundle.style.height = height;
-    text_input_bundle.style.width = width;
-}
+//     let height = match size {
+//         Some(TextInputSize::Small) => size_small_height,
+//         Some(TextInputSize::Normal) => size_normal_height,
+//         Some(TextInputSize::Large) => size_large_height,
+//         None => size_normal_height,
+//     };
 
-fn get_default_text_input_style(border_width: UiRect) -> Style {
-    Style {
+//     text_input_bundle.style.height = height;
+//     text_input_bundle.style.width = width;
+// }
+
+// fn get_default_text_input_style(border_width: UiRect) -> Style {
+//     Style {
+//         justify_content: JustifyContent::Start,
+//         align_items: AlignItems::Center,
+//         border: border_width,
+//         padding: UiRect {
+//             left: Val::Px(5.0),
+//             right: Val::Px(5.0),
+//             top: Val::Px(1.0),
+//             bottom: Val::Px(1.0),
+//         },
+//         ..default()
+//     }
+// }
+
+// pub fn default_text_input_bundle(
+//     border_width: UiRect,
+//     border_radius: BorderRadius,
+//     size: &Option<TextInputSize>,
+// ) -> FaWidgetBundle {
+//     let mut input_bundle = FaWidgetBundle {
+//         style: get_default_text_input_style(border_width),
+//         border_radius,
+//         border_color: BorderColor(Color::srgba(0.902, 0.902, 0.902, 0.922)),
+//         ..default()
+//     };
+//     set_text_input_default_size(size, &mut input_bundle);
+//     input_bundle
+// }
+
+pub fn default_input_node(border_width: UiRect) -> Node {
+    Node {
         justify_content: JustifyContent::Start,
         align_items: AlignItems::Center,
         border: border_width,
         padding: UiRect {
             left: Val::Px(5.0),
             right: Val::Px(5.0),
-            top: Val::Px(1.0),
-            bottom: Val::Px(1.0),
+            top: Val::Px(4.0),
+            bottom: Val::Px(4.0),
         },
+        height: Val::Auto,
+        width: Val::Percent(100.0),
         ..default()
     }
-}
-
-pub fn default_text_input_bundle(
-    border_width: UiRect,
-    border_radius: BorderRadius,
-    size: &Option<TextInputSize>,
-) -> FaWidgetBundle {
-    let mut input_bundle = FaWidgetBundle {
-        style: get_default_text_input_style(border_width),
-        border_radius,
-        border_color: BorderColor(Color::srgba(0.902, 0.902, 0.902, 0.922)),
-        ..default()
-    };
-    set_text_input_default_size(size, &mut input_bundle);
-    input_bundle
 }
 
 pub fn outlined_border_width() -> UiRect {
@@ -72,4 +92,19 @@ pub fn outlined_border_radius() -> BorderRadius {
 
 pub fn underlined_border_radius() -> BorderRadius {
     BorderRadius::all(Val::Px(0.0))
+}
+
+use super::TextInputSize;
+
+pub fn get_text_size(size: &TextInputSize) -> f32 {
+    let size_small = 16.0;
+    let size_normal = 20.0;
+    let size_large = 24.0;
+
+    let text_size = match size {
+        TextInputSize::Small => size_small,
+        TextInputSize::Normal => size_normal,
+        TextInputSize::Large => size_large,
+    };
+    text_size
 }
--- src/widgets/text_input/mod.rs
+++ src/widgets/text_input/mod.rs
@@ -1,20 +1,13 @@
 pub mod helper;
-pub mod text_value;
 
 use bevy::utils::HashMap;
 use helper::*;
-pub use text_value::*;
 
 use crate::utils;
-use crate::widgets::{DefaultTextBundle, DefaultWidgetBundle};
+use crate::widgets::{DefaultTextEntity, DefaultWidgetEntity, FamiqWidgetId};
 use bevy::ecs::system::EntityCommands;
 use bevy::prelude::*;
 use smol_str::SmolStr;
-
-use super::{
-    selection::styling::{outlined_border_radius, underlined_border_radius},
-    FamiqWidgetId,
-};
 
 #[derive(Component)]
 pub struct TextInput {
@@ -34,11 +27,11 @@ impl TextInput {
 }
 
 #[derive(Resource, Debug)]
-pub struct TextInputResource {
+pub struct FaTextInputResource {
     pub inputs: HashMap<String, String>,
 }
 
-impl TextInputResource {
+impl FaTextInputResource {
     pub fn update_or_insert(&mut self, id: String, new_value: String) {
         if let Some(old_value) = self.inputs.get_mut(&id) {
             *old_value = new_value;
@@ -65,57 +58,40 @@ pub enum TextInputSize {
 
 pub struct FaTextInput;
 
-// need container
 impl<'a> FaTextInput {
-    fn _build_text_input(
+    fn _build_placeholder(
         id: &str,
         placeholder: &str,
         root_node: &'a mut EntityCommands,
         asset_server: &'a ResMut<'a, AssetServer>,
         font_path: &String,
-        size: Option<TextInputSize>,
-        border_width: UiRect,
-        border_radius: BorderRadius,
+        size: &TextInputSize,
     ) -> Entity {
-        let input_bundle = default_text_input_bundle(border_width, border_radius, &size);
-        let text_bundle = create_text_input_value(placeholder, &size, asset_server, font_path);
+        let txt = Text::new(placeholder);
+        let txt_font = TextFont {
+            font: asset_server.load(utils::strip_assets_prefix(font_path).unwrap()),
+            font_size: get_text_size(size),
+            ..default()
+        };
+        let txt_color = TextColor(PLACEHOLDER_COLOR);
+        let txt_layout = TextLayout::new_with_justify(JustifyText::Center);
 
-        let text_entity = root_node
+        root_node
             .commands()
             .spawn((
-                text_bundle,
-                FamiqWidgetId(format!("{id}_text_input_value")),
-                DefaultTextBundle(create_text_input_value(
-                    placeholder,
-                    &size,
-                    asset_server,
-                    font_path,
-                )),
+                txt.clone(),
+                txt_font.clone(),
+                txt_color.clone(),
+                txt_layout.clone(),
+                FamiqWidgetId(format!("{id}_placeholder")),
+                DefaultTextEntity::new(txt, txt_font, txt_color, txt_layout),
             ))
-            .id();
-
-        let input_entity = root_node
-            .commands()
-            .spawn((
-                input_bundle.clone(),
-                FamiqWidgetId(id.to_string()),
-                TextInput::new("", placeholder),
-                IsFamiqTextInput,
-                DefaultWidgetBundle(input_bundle),
-            ))
-            .id();
-
-        utils::entity_add_child(root_node, text_entity, input_entity);
-        input_entity
+            .id()
     }
 
-    pub fn fa_text_input(
+    fn _build_input(
         id: &str,
-        placeholder: &str,
         root_node: &'a mut EntityCommands,
-        asset_server: &'a ResMut<'a, AssetServer>,
-        font_path: &String,
-        size: Option<TextInputSize>,
         variant: TextInputVariant,
     ) -> Entity {
         let mut border_width = outlined_border_width();
@@ -128,100 +104,209 @@ impl<'a> FaTextInput {
             }
             _ => (),
         }
+        let node = default_input_node(border_width);
+        let border_color = BorderColor(Color::srgba(0.902, 0.902, 0.902, 0.922));
+        let bg_color = BackgroundColor::default();
+        let z_index = ZIndex::default();
+        let visibility = Visibility::Visible;
 
-        Self::_build_text_input(
-            id,
-            placeholder,
-            root_node,
-            asset_server,
-            font_path,
-            size,
-            border_width,
-            border_radius,
-        )
+        root_node
+            .commands()
+            .spawn((
+                node.clone(),
+                border_color.clone(),
+                bg_color.clone(),
+                border_radius.clone(),
+                z_index.clone(),
+                visibility.clone(),
+                FamiqWidgetId(id.to_string()),
+                IsFamiqTextInput,
+                DefaultWidgetEntity::new(
+                    node,
+                    border_color,
+                    border_radius,
+                    bg_color,
+                    z_index,
+                    visibility,
+                ),
+            ))
+            .id()
     }
 
-    fn update_input_text_bundle_color(
-        children: &Children,
-        text_query: &mut Query<&mut Text>,
-        color: Color,
-    ) {
-        for &child in children.iter() {
-            if let Ok(mut text) = text_query.get_mut(child) {
-                text.sections[0].style.color = color;
-            }
-        }
-    }
+    pub fn new(
+        id: &str,
+        ph: &str, // placeholder
+        root_node: &'a mut EntityCommands,
+        asset_server: &'a ResMut<'a, AssetServer>,
+        font_path: &String,
+        size: TextInputSize,
+        variant: TextInputVariant,
+    ) -> Entity {
+        let input = Self::_build_input(id, root_node, variant);
+        let ph = Self::_build_placeholder(id, ph, root_node, asset_server, font_path, &size);
 
-    pub fn update_input_text_bundle_value(
-        children: &Children,
-        text_query: &mut Query<&mut Text>,
-        text_input: &Mut<TextInput>,
-    ) {
-        for &child in children.iter() {
-            if let Ok(mut text) = text_query.get_mut(child) {
-                if text_input.text == "" {
-                    text.sections[0].value = text_input.placeholder.clone();
-                } else {
-                    text.sections[0].value = text_input.text.clone();
-                }
-            }
-        }
-    }
-
-    pub fn set_unfocused_all(
-        text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
-        text_q: &mut Query<&mut Text>,
-        input_resource: &mut ResMut<TextInputResource>,
-    ) {
-        for (children, mut input, id) in text_input_q.iter_mut() {
-            input.focused = false;
-            input_resource.update_or_insert(id.0.clone(), input.text.clone());
-            Self::update_input_text_bundle_color(&children, text_q, PLACEHOLDER_COLOR);
-        }
-    }
-
-    pub fn set_focus(
-        text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
-        text_input_entity: Entity,
-        text_q: &mut Query<&mut Text>,
-        input_resource: &mut ResMut<TextInputResource>,
-    ) {
-        if let Ok((children, mut input, id)) = text_input_q.get_mut(text_input_entity) {
-            input.focused = true;
-            input_resource.update_or_insert(id.0.clone(), input.text.clone());
-            Self::update_input_text_bundle_color(&children, text_q, TEXT_INPUT_VALUE_COLOR);
-        }
-    }
-
-    pub fn add_text(
-        text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
-        text_q: &mut Query<&mut Text>,
-        input: &SmolStr,
-        input_resource: &mut ResMut<TextInputResource>,
-    ) {
-        for (children, mut text_input, id) in text_input_q.iter_mut() {
-            if text_input.focused {
-                text_input.text.push_str(input);
-                input_resource.update_or_insert(id.0.clone(), text_input.text.clone());
-                Self::update_input_text_bundle_value(&children, text_q, &text_input);
-                break;
-            }
-        }
-    }
-
-    pub fn delete_text(
-        text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
-        text_q: &mut Query<&mut Text>,
-        input_resource: &mut ResMut<TextInputResource>,
-    ) {
-        for (children, mut text_input, id) in text_input_q.iter_mut() {
-            if text_input.focused {
-                text_input.text.pop();
-                input_resource.update_or_insert(id.0.clone(), text_input.text.clone());
-                Self::update_input_text_bundle_value(&children, text_q, &text_input);
-                break;
-            }
-        }
+        utils::entity_add_child(root_node, ph, input);
+        input
     }
 }
+
+// // need container
+// impl<'a> FaTextInput {
+//     fn _build_text_input(
+//         id: &str,
+//         placeholder: &str,
+//         root_node: &'a mut EntityCommands,
+//         asset_server: &'a ResMut<'a, AssetServer>,
+//         font_path: &String,
+//         size: Option<TextInputSize>,
+//         border_width: UiRect,
+//         border_radius: BorderRadius,
+//     ) -> Entity {
+//         let input_bundle = default_text_input_bundle(border_width, border_radius, &size);
+//         let text_bundle = create_text_input_value(placeholder, &size, asset_server, font_path);
+
+//         let text_entity = root_node
+//             .commands()
+//             .spawn((
+//                 text_bundle,
+//                 FamiqWidgetId(format!("{id}_text_input_value")),
+//                 DefaultTextBundle(create_text_input_value(
+//                     placeholder,
+//                     &size,
+//                     asset_server,
+//                     font_path,
+//                 )),
+//             ))
+//             .id();
+
+//         let input_entity = root_node
+//             .commands()
+//             .spawn((
+//                 input_bundle.clone(),
+//                 FamiqWidgetId(id.to_string()),
+//                 TextInput::new("", placeholder),
+//                 IsFamiqTextInput,
+//                 DefaultWidgetBundle(input_bundle),
+//             ))
+//             .id();
+
+//         utils::entity_add_child(root_node, text_entity, input_entity);
+//         input_entity
+//     }
+
+//     pub fn fa_text_input(
+//         id: &str,
+//         placeholder: &str,
+//         root_node: &'a mut EntityCommands,
+//         asset_server: &'a ResMut<'a, AssetServer>,
+//         font_path: &String,
+//         size: Option<TextInputSize>,
+//         variant: TextInputVariant,
+//     ) -> Entity {
+//         let mut border_width = outlined_border_width();
+//         let mut border_radius = outlined_border_radius();
+
+//         match variant {
+//             TextInputVariant::Underlined => {
+//                 border_width = underlined_border_width();
+//                 border_radius = underlined_border_radius();
+//             }
+//             _ => (),
+//         }
+
+//         Self::_build_text_input(
+//             id,
+//             placeholder,
+//             root_node,
+//             asset_server,
+//             font_path,
+//             size,
+//             border_width,
+//             border_radius,
+//         )
+//     }
+
+//     fn update_input_text_bundle_color(
+//         children: &Children,
+//         text_query: &mut Query<&mut Text>,
+//         color: Color,
+//     ) {
+//         for &child in children.iter() {
+//             if let Ok(mut text) = text_query.get_mut(child) {
+//                 text.sections[0].style.color = color;
+//             }
+//         }
+//     }
+
+//     pub fn update_input_text_bundle_value(
+//         children: &Children,
+//         text_query: &mut Query<&mut Text>,
+//         text_input: &Mut<TextInput>,
+//     ) {
+//         for &child in children.iter() {
+//             if let Ok(mut text) = text_query.get_mut(child) {
+//                 if text_input.text == "" {
+//                     text.sections[0].value = text_input.placeholder.clone();
+//                 } else {
+//                     text.sections[0].value = text_input.text.clone();
+//                 }
+//             }
+//         }
+//     }
+
+//     pub fn set_unfocused_all(
+//         text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
+//         text_q: &mut Query<&mut Text>,
+//         input_resource: &mut ResMut<TextInputResource>,
+//     ) {
+//         for (children, mut input, id) in text_input_q.iter_mut() {
+//             input.focused = false;
+//             input_resource.update_or_insert(id.0.clone(), input.text.clone());
+//             Self::update_input_text_bundle_color(&children, text_q, PLACEHOLDER_COLOR);
+//         }
+//     }
+
+//     pub fn set_focus(
+//         text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
+//         text_input_entity: Entity,
+//         text_q: &mut Query<&mut Text>,
+//         input_resource: &mut ResMut<TextInputResource>,
+//     ) {
+//         if let Ok((children, mut input, id)) = text_input_q.get_mut(text_input_entity) {
+//             input.focused = true;
+//             input_resource.update_or_insert(id.0.clone(), input.text.clone());
+//             Self::update_input_text_bundle_color(&children, text_q, TEXT_INPUT_VALUE_COLOR);
+//         }
+//     }
+
+//     pub fn add_text(
+//         text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
+//         text_q: &mut Query<&mut Text>,
+//         input: &SmolStr,
+//         input_resource: &mut ResMut<TextInputResource>,
+//     ) {
+//         for (children, mut text_input, id) in text_input_q.iter_mut() {
+//             if text_input.focused {
+//                 text_input.text.push_str(input);
+//                 input_resource.update_or_insert(id.0.clone(), text_input.text.clone());
+//                 Self::update_input_text_bundle_value(&children, text_q, &text_input);
+//                 break;
+//             }
+//         }
+//     }
+
+//     pub fn delete_text(
+//         text_input_q: &mut Query<(&Children, &mut TextInput, &FamiqWidgetId)>,
+//         text_q: &mut Query<&mut Text>,
+//         input_resource: &mut ResMut<TextInputResource>,
+//     ) {
+//         for (children, mut text_input, id) in text_input_q.iter_mut() {
+//             if text_input.focused {
+//                 text_input.text.pop();
+//                 input_resource.update_or_insert(id.0.clone(), text_input.text.clone());
+//                 Self::update_input_text_bundle_value(&children, text_q, &text_input);
+//                 break;
+//             }
+//         }
+//     }
+// }
