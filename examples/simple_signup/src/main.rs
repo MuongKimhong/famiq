bcfd626b217b0e1a5185e33af381327557df169b
MuongKimhong <muongkimhong086@gmail.com>
2025-01-03 15:17:51 +1100

    update example for bevy 0.15

--- examples/simple_signup/src/main.rs
+++ examples/simple_signup/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(famiq_plugin) // add plugin
         .add_systems(Startup, setup)
-        // .add_systems(Update, handle_btn_press)
         .run();
 }
 
@@ -17,80 +16,36 @@ fn setup(
 ) {
     commands.spawn(Camera2d::default());
 
-    // create a builder
-    let mut ui = FamiqWidgetBuilder::new(
+    // create a widget builder
+    let mut builder = FamiqWidgetBuilder::new(
         &mut commands,
         &asset_server,
         &mut builder_resource,
-        "assets/fonts/fira-mono-regular.ttf", // should be inside assets folder & outside src folder
-        "assets/styles.json",                 // should be inside assets folder & outside src folder
-        true,                                 // hot-reload, useful during development
+        "assets/fonts/fira-mono-regular.ttf",
+        "assets/styles.json",
+        true,
     );
 
-    // fa_fps
-    // ui.fa_fps("#fps-text");
+    let txt = builder.fa_text("#signup-txt", "SIGN UP");
 
-    // // fa_text needs container
-    // let txt = ui.fa_text("#signup-txt", "SIGN UP");
-    // let txt_container = ui.fa_container("#signup-txt-container", &vec![txt]);
+    let f_name = builder.fa_text_input("#f_name", "First name", "", "");
+    let l_name = builder.fa_text_input("#l_name", "Last name", "", "");
+    let name_container = builder.fa_container("#name-container", &vec![f_name, l_name]);
 
-    // // fa_text_input needs container
-    // let f_name = ui.fa_text_input("#f_name", "First name", None, None);
-    // let l_name = ui.fa_text_input("#l_name", "Last name", None, None);
-    // let name_container = ui.fa_container("#name-container", &vec![f_name, l_name]);
+    let ask = builder.fa_selection(
+        "#ask",
+        "Select your subscription",
+        &vec!["Personal".to_string(), "Team".to_string()],
+        "",
+        "",
+        "",
+    );
+    let ask_container = builder.fa_container("#ask-container", &vec![ask]);
 
-    // // fa_selection doesn't need containers
-    // let choices = vec!["Personal".to_string(), "Team".to_string()];
-    // let ask = ui.fa_selection(
-    //     "#ask",
-    //     "Select your subscription",
-    //     &choices,
-    //     None,
-    //     None,
-    //     None,
-    // );
+    let btn = builder.fa_button("#btn", "Confirm", "", "");
 
-    // fa_button needs container
-    ui.fa_fps("#fpstext");
-    let txt = ui.fa_text("#txt", "Hello world");
-    let btn = ui.fa_button("#btn", "Confirm", "secondary", "");
-    let btn_one = ui.fa_button("#btn", "Confirm", "warning", "normal");
-    let btn_two = ui.fa_button("#btn", "Confirm", "danger", "large");
-    let input = ui.fa_text_input("#input", "Enter your name", "", "");
-
-    let choices = vec!["hello".to_string(), "world".to_string()];
-    let selection = ui.fa_selection("#hello", "Select", &choices, "", "", "");
-    let btn_container = ui.fa_container("#btn-container", &vec![btn, btn_one, btn_two, txt, input, selection]);
-
-    // ui.fa_container(
-    //     "#container",
-    //     &vec![txt_container, name_container, ask, btn_container],
-    // );
+    builder.fa_container(
+        "#container",
+        &vec![txt, name_container, ask_container, btn],
+    );
 }
-
-// fn handle_btn_press(
-//     mut events: EventReader<FaInteractionEvent>,
-//     selected_items: Res<SelectedItemsResource>,
-//     input_values: Res<TextInputResource>,
-// ) {
-//     for e in events.read() {
-//         if e.widget_type == WidgetType::Button && e.interaction_type == Interaction::Pressed {
-//             match e.widget_id.as_str() {
-//                 "#btn" => {
-//                     if let Some(selected) = selected_items.items.get("#ask") {
-//                         println!("selected choice: {:?}", selected);
-//                     }
-
-//                     if let Some(first_name) = input_values.inputs.get("#f_name") {
-//                         println!("Frist name: {:?}", first_name);
-//                     }
-
-//                     if let Some(last_name) = input_values.inputs.get("#l_name") {
-//                         println!("Last name: {:?}", last_name);
-//                     }
-//                 }
-//                 _ => (),
-//             }
-//         }
-//     }
-// }
