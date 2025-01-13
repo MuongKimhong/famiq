889c7bb65ab010c8710abb5df755f843ae3c4ba9
MuongKimhong <muongkimhong086@gmail.com>
2025-01-13 21:28:22 +1100

    Partially fixed Listview bugs; further debugging required.

--- src/widgets/list_view/helper.rs
+++ src/widgets/list_view/helper.rs
@@ -3,9 +3,18 @@ use bevy::prelude::*;
 pub fn default_move_panel_node() -> Node {
     Node {
         width: Val::Percent(100.0),
+        position_type: PositionType::Absolute,
+        left: Val::Px(0.0),
+        top: Val::Px(0.0),
         flex_direction: FlexDirection::Column,
         align_items: AlignItems::FlexStart,
         justify_content: JustifyContent::FlexStart,
+        padding: UiRect {
+            left: Val::Px(0.0),
+            right: Val::Px(0.0),
+            top: Val::Px(5.0),
+            bottom: Val::Px(5.0)
+        },
         ..default()
     }
 }
@@ -18,6 +27,7 @@ pub fn default_listview_node() -> Node {
         justify_content: JustifyContent::FlexStart,
         height: Val::Percent(50.0),
         overflow: Overflow::clip(),
+        padding: UiRect::all(Val::Px(0.0)),
         ..default()
     }
 }
--- src/widgets/list_view/mod.rs
+++ src/widgets/list_view/mod.rs
@@ -62,7 +62,7 @@ impl<'a> FaListView {
         let border_color = BorderColor::default();
         let border_radius = BorderRadius::default();
         let z_index = ZIndex::default();
-        let visibility = Visibility::Visible;
+        let visibility = Visibility::Inherited;
 
         let move_panel_entity = root_node
             .commands()
@@ -83,8 +83,7 @@ impl<'a> FaListView {
                     z_index,
                     visibility,
                 ),
-                ScrollList::default(),
-
+                ScrollList::default()
             ))
             .id();
 
@@ -100,7 +99,12 @@ impl<'a> FaListView {
         move_panel_entity
     }
 
-    fn _build_listview(id: &str, classes: &str, root_node: &'a mut EntityCommands, panel_entity: Entity) -> Entity {
+    fn _build_listview(
+        id: &str,
+        classes: &str,
+        root_node: &'a mut EntityCommands,
+        panel_entity: Entity,
+    ) -> Entity {
         let node = default_listview_node();
         let bg_color = BackgroundColor::default();
         let border_color = BorderColor::default();
@@ -108,7 +112,7 @@ impl<'a> FaListView {
         let z_index = ZIndex::default();
         let visibility = Visibility::Visible;
 
-        root_node
+        let listview_entity = root_node
             .commands()
             .spawn((
                 node.clone(),
@@ -129,9 +133,10 @@ impl<'a> FaListView {
                     visibility,
                 ),
                 Interaction::default(),
-                ListViewMovePanelEntity(panel_entity)
+                ListViewMovePanelEntity(panel_entity),
             ))
-            .id()
+            .id();
+        listview_entity
     }
 
     pub fn new(id: &str, classes: &str, root_node: &'a mut EntityCommands, items: &Vec<Entity>) -> Entity {
@@ -153,16 +158,15 @@ impl<'a> FaListView {
         Some((padding_top.unwrap(), padding_bottom.unwrap()))
     }
 
-    fn _calculate_max_scroll(panel_node: &ComputedNode, listview_node: &ComputedNode, listview_style: &Node) -> f32 {
+    fn _calculate_max_scroll(
+        panel_node: &ComputedNode,
+        listview_node: &ComputedNode,
+        // listview_style: &Node,
+    ) -> f32 {
         let panel_height = panel_node.size().y;
         let container_height = listview_node.size().y;
 
-        let mut max_scroll = panel_height - container_height;
-
-        let padding_y = Self::_calculate_listview_padding_y(listview_style);
-        if let Some((top, bottom)) = padding_y {
-            max_scroll += top + bottom;
-        }
+        let max_scroll = panel_height - container_height;
         max_scroll.max(0.0)
     }
 
@@ -178,74 +182,43 @@ impl<'a> FaListView {
         }
     }
 
-    pub fn on_mouse_scroll_system(
+    pub fn on_scroll_system(
         mut mouse_wheel_events: EventReader<MouseWheel>,
-        listview_q: Query<
-            (&ComputedNode, &Node, &ListViewMovePanelEntity),
-            Without<ScrollList>
-        >,
-        mut listview_panel_q: Query<(&mut ScrollList, &mut Node, &mut DefaultWidgetEntity, &ComputedNode)>,
+        mut listview_q: Query<(&mut Node, &ComputedNode, &ListViewMovePanelEntity), Without<ScrollList>>,
+        mut panel_q: Query<(&mut Node, &ComputedNode, &mut ScrollList, &mut DefaultWidgetEntity)>,
         can_be_scrolled_listview: ResMut<CanBeScrolledListView>,
     ) {
-        let scroll_height = 12.0;
+        let scroll_height: f32 = 12.0;
+
+        for (mut listview_node, _, _) in listview_q.iter_mut() {
+            // always set paddings to 0 as ListView can't have paddings.
+            listview_node.padding = UiRect::all(Val::Px(0.0));
+        }
 
         for e in mouse_wheel_events.read() {
             if let Some(hovered_listview) = can_be_scrolled_listview.entity {
-                if let Ok((l_node, l_style, panel_entity)) = listview_q.get(hovered_listview) {
-                    if let Ok((mut scrolling, mut style, mut default_widget, panel_node)) = listview_panel_q.get_mut(panel_entity.0)
-                    {
-                        let max_scroll = Self::_calculate_max_scroll(panel_node, l_node, l_style);
+
+                // get hovered listview
+                if let Ok((_, listview_c_node, panel_entity)) = listview_q.get_mut(hovered_listview) {
+
+                    // get panel
+                    if let Ok((mut panel_node, panel_c_node, mut scroll_list, mut default_style)) = panel_q.get_mut(panel_entity.0) {
 
                         let dy = match e.unit {
                             MouseScrollUnit::Line => e.y * scroll_height,
                             MouseScrollUnit::Pixel => e.y,
                         };
-                        scrolling.position += dy;
-                        scrolling.position = scrolling.position.clamp(-max_scroll, 0.);
-                        style.top = Val::Px(scrolling.position);
-                        default_widget.node.top = Val::Px(scrolling.position);
+                        let max_scroll = Self::_calculate_max_scroll(panel_c_node, listview_c_node);
+
+                        scroll_list.position = (scroll_list.position + dy).clamp(-max_scroll, 0.0);
+
+                        panel_node.top = Val::Px(scroll_list.position);
+                        default_style.node.top = Val::Px(scroll_list.position);
+
                     }
+
                 }
-            }
-        }
-    }
 
-    pub fn scroll_top(
-        listview_entity: Entity,
-        listview_q: &Query<(&ComputedNode, &Node, &ListViewMovePanelEntity)>,
-        listview_panel_q: &mut Query<(&mut ScrollList, &mut Node, &ComputedNode)>,
-    ) {
-        if let Ok((_, listview_style, panel_entity)) = listview_q.get(listview_entity) {
-            if let Ok((mut list, mut style, _)) = listview_panel_q.get_mut(panel_entity.0) {
-                match Self::_calculate_listview_padding_y(listview_style) {
-                    Some((top, _)) => list.position = top,
-                    None => list.position = 0.0,
-                }
-                style.top = Val::Px(list.position);
-            }
-        }
-    }
-
-    pub fn scroll_bottom(
-        listview_entity: Entity,
-        listview_q: &Query<(&ComputedNode, &Node, &ListViewMovePanelEntity)>,
-        listview_panel_q: &mut Query<(&mut ScrollList, &mut Node, &ComputedNode)>,
-    ) {
-        if let Ok((listview_node, listview_style, panel_entity)) = listview_q.get(listview_entity) {
-            if let Ok((mut list, mut style, panel_node)) = listview_panel_q.get_mut(panel_entity.0)
-            {
-                let listview_height = listview_node.size().y;
-                let panel_height = panel_node.size().y;
-
-                list.position = panel_height - listview_height;
-
-                match Self::_calculate_listview_padding_y(listview_style) {
-                    Some((top, bottom)) => {
-                        list.position += top + bottom;
-                    }
-                    None => (),
-                }
-                style.top = Val::Px(list.position.max(0.0));
             }
         }
     }
