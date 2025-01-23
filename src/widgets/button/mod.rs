a5153483ed6ea1c345dbd77f373c3a2ed5e637a0
MuongKimhong <muongkimhong086@gmail.com>
2025-01-24 10:53:33 +1100

    added btn test

--- src/widgets/button/mod.rs
+++ src/widgets/button/mod.rs
@@ -284,3 +284,83 @@ pub fn fa_button<'a>(builder: &'a mut FamiqWidgetBuilder, text: &str) -> FaButto
 pub fn can_run_button_systems(builder_res: Res<FamiqWidgetResource>) -> bool {
     builder_res.can_run_systems.button
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::plugin::FamiqPlugin;
+    use bevy::asset::AssetPlugin;
+    use super::*;
+
+    fn create_test_app() -> App {
+        let mut app = App::new();
+        // Note the use of `MinimalPlugins` instead of `DefaultPlugins`, as described above.
+        app.add_plugins(MinimalPlugins);
+        app.add_plugins(AssetPlugin::default());
+        app.init_asset::<Font>();
+        // Spawning a fake window allows testing systems that require a window.
+        app.world_mut().spawn(Window::default());
+        app
+    }
+
+    fn setup_test_default_button(
+        mut commands: Commands,
+        asset_server: ResMut<AssetServer>,
+        mut builder_res: ResMut<FamiqWidgetResource>,
+    ) {
+        let mut builder = FamiqWidgetBuilder::new(&mut commands, &mut builder_res, &asset_server);
+        fa_button(&mut builder, "Press me").id("#test-btn").build();
+    }
+
+    fn setup_test_button_with_built_in_class(
+        mut commands: Commands,
+        asset_server: ResMut<AssetServer>,
+        mut builder_res: ResMut<FamiqWidgetResource>,
+    ) {
+        let mut builder = FamiqWidgetBuilder::new(&mut commands, &mut builder_res, &asset_server);
+        fa_button(&mut builder, "Press me")
+            .id("#test-btn")
+            .class("is-primary is-large is-round")
+            .build();
+    }
+
+    #[test]
+    fn test_create_default_button() {
+        let mut app = create_test_app();
+        app.add_plugins(FamiqPlugin);
+        app.insert_resource(FamiqWidgetResource::default());
+        app.add_systems(Startup, setup_test_default_button);
+        app.update();
+
+        let btn_q = app.world_mut().query::<(&FamiqWidgetId, &IsFamiqButton)>().get_single(app.world());
+        assert!(btn_q.is_ok(), "There should be only 1 button");
+
+        let btn_id = btn_q.unwrap().0;
+        assert_eq!(
+            "#test-btn".to_string(),
+            btn_id.0
+        );
+
+        let btn_text_q = app.world_mut().query::<(&Text, &IsFamiqButtonText)>()
+                        .get_single(app.world());
+
+        assert_eq!(
+            "Press me".to_string(),
+            btn_text_q.unwrap().0.0
+        );
+    }
+
+    #[test]
+    fn test_create_button_with_built_in_class() {
+        let mut app = create_test_app();
+        app.add_plugins(FamiqPlugin);
+        app.insert_resource(FamiqWidgetResource::default());
+        app.add_systems(Startup, setup_test_button_with_built_in_class);
+        app.update();
+
+        let btn_q = app.world_mut().query::<(&FamiqWidgetClasses, &IsFamiqButton)>().get_single(app.world());
+        assert_eq!(
+            "is-primary is-large is-round".to_string(),
+            btn_q.unwrap().0.0
+        );
+    }
+}
